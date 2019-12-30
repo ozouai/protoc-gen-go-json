74d45db34002ca8698897be1c48ff8d1e19f395c
Omar Zouai <omar@visor.com>
2019-12-30 09:54:15 -0800

    Correct import

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/codegenerator"
 	"github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway/descriptor"
 
-	"github.com/visor-tax/protoc-gen-go-json/gen"
+	"github.com/mitchellh/protoc-gen-go-json/gen"
 )
 
 var (
